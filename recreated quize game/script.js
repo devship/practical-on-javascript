7977bb30d5570afa20d2b916a753a26d28b2298a
Gabriella <gabriellaarchibong@gmail.com>
2023-11-27 10:08:20 +0100

    Update script.js

--- recreated quize game/script.js
+++ recreated quize game/script.js
@@ -2,7 +2,7 @@
 const start = document.getElementById('start');
 const quize = document.getElementById('quiz');
 const qimg = document.getElementById('question-img');
-const question = document.getElementById('question');
+const questionElement = document.getElementById('question');
 const timeQauge = document.getElementById('timequage');
 console.log(timeQauge);
 
@@ -11,3 +11,30 @@ const choiceB = document.getElementById('choice-2');
 const choiceC = document.getElementById('choice-3');
 
 const progress = document.getElementById('progress');
+const score = document.getElementById('score-container');
+
+let  = [
+    {
+        question : 'what does html stands for',
+        imgSrc : 'images/images.png',
+        choiceA : 'hypertext markup language',
+        choiceB : 'hyperlinks and text markup language',
+        choiceC : 'home tool and markup language',
+        correct : 'A'
+    },
+    {
+        question : 'what is the questio #2',
+        imgSrc : 'images/images.png',
+        choiceA : 'A',
+        choiceB : 'B',
+        choiceC : 'c',
+        correct : 'a'
+    }
+];
+
+let lastQuestionIndex = questions.length -1;
+let runningQuestionIndex = 0;
+function renderedQuestion () {
+    let q = questions[runningQuestionIndex];
+    qimg.innerHTML = `< img scr = ${q.imgSrc}>`;0
+}
